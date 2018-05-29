0f94d3e76d2a217d0052f73c0f01c86c09d129d7
Johannes Lohner <johannes.lohner@hotmail.com>
2018-05-29 18:36:37 +0200

    update edit page - didn't get anywhere

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,10 @@
 import "bootstrap";
+import changeNavbarScroll from "./navbar";
+import "./map";
 
 import scaleVideoContainer from "./banner"
 import initBannerVideoSize from "./banner"
 import scaleBannerVideoSize from "./banner"
-import changeNavbarScroll from "./navbar";
 
-import "./map";
-import changeNavbarScroll from "./navbar";
 
 
