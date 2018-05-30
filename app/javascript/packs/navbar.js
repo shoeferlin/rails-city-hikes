9ac4f9fdb81436a93533fa03feb4b0b78248c670
Nathan Lebas <nathanlebas3@gmail.com>
2018-05-30 11:47:49 +0200

    changed, the navbar background color and the font color

--- app/javascript/packs/navbar.js
+++ app/javascript/packs/navbar.js
@@ -2,7 +2,7 @@ function changeNavbarScroll() {
   console.log(window.scrollY)
   const navbar = document.querySelector(".navbar-wagon");
   if(window.scrollY > 693) {
-    navbar.classList.add("navbar-solid")
+    navbar.classList.add("navbar-solid") && navbar.classList.add("navbar-solid")
   } else {
     navbar.classList.remove("navbar-solid")
   }
@@ -11,5 +11,17 @@ function changeNavbarScroll() {
 window.addEventListener("scroll", changeNavbarScroll);
 
 export default changeNavbarScroll;
+
+
+
+function changeNavbarFontScroll() {
+  console.log(window.scrollY)
+  const navbar = document.querySelector(".navbar-wagon");
+  if(window.scrollY > 693) {
+    navbar.classList.add("navbar-solid")
+  } else {
+    navbar.classList.remove("navbar-solid")
+  }
+}
 
 
