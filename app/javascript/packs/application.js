351fa487526b020c23283496605a85498481e7ff
Simon Hoeferlin <s@hoeferlins.de>
2018-05-30 14:53:11 +0200

    Added favicon and icon

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,5 +5,6 @@ import "./map";
 import scaleVideoContainer from "./banner"
 import initBannerVideoSize from "./banner"
 import scaleBannerVideoSize from "./banner"
+import playingVideo from "./banner"
 
 
