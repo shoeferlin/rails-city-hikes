2972423bbb593e5c746802fd9f8366a5e8055f68
Felix Fastrich <fastrichfelix@gmail.com>
2018-06-05 10:52:22 +0200

    updated landing page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,6 +1,8 @@
 import "bootstrap";
 import changeNavbarScroll from "./navbar";
 import "./map";
+import "./timeline";
+import "./carousel";
 import "../components/getinfo"
 // import "../components/draganddrop.js"
 // import { Sortable } from '@shopify/draggable';
@@ -10,7 +12,6 @@ import { draganddrop } from '../components/draganddrop';
 import scaleVideoContainer from "./banner"
 import initBannerVideoSize from "./banner"
 import scaleBannerVideoSize from "./banner"
-import playingVideo from "./banner"
 
 
 
